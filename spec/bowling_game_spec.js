1a8d2818666fdd1fbd3a00da51cb3bff54039eb3
Chris Nelson <superchrisnelson@gmail.com>
2015-05-04 13:00:15 -0400

    oops

--- spec/bowling_game_spec.js
+++ spec/bowling_game_spec.js
@@ -2,6 +2,6 @@ import Game from "./game.es6.js"
 
 describe("Game", function() {
   it("should calculate simple scores", function() {
-    expect(new Game([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).score()).toEqual(45);
+    expect(new Game([0, 1, 2, 3, 4, 0, 1, 2, 3, 4]).score()).toEqual(20);
   });
 });
